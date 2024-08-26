c3699b270bf6eb98720f1b544a60ac61a80e1050
rotsyrotsy <rafa.rotsy@gmail.com>
2024-08-26 23:31:16 +0300

    [Image] compress images

--- app/src/main/java/com/example/trocapp/service/ImageLoader.java
+++ app/src/main/java/com/example/trocapp/service/ImageLoader.java
@@ -13,6 +13,23 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ImageLoader {
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
+    }
 
     public static void setImageFromUrl(final ImageView imageView, final String urlString) {
         ExecutorService executor = Executors.newSingleThreadExecutor();
@@ -26,10 +43,27 @@ public class ImageLoader {
                 connection.setDoInput(true);
                 connection.connect();
                 InputStream input = connection.getInputStream();
-                Bitmap bitmap = BitmapFactory.decodeStream(input);
+                //Bitmap bitmap = BitmapFactory.decodeStream(input);
+                //handler.post(() -> imageView.setImageBitmap(bitmap));
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inJustDecodeBounds = true;
+                BitmapFactory.decodeStream(input, null, options);
+                System.out.println( "Image dimensions: " + options.outWidth + "x" + options.outHeight);
 
-                // Set the bitmap on the main thread
+                // Calculate inSampleSize
+                options.inSampleSize = calculateInSampleSize(options, imageView.getWidth(), imageView.getHeight());
+                options.inJustDecodeBounds = false;
+
+                // Restart the input stream since the first one was consumed
+                input.close();
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                input = connection.getInputStream();
+
+                Bitmap bitmap = BitmapFactory.decodeStream(input, null, options);
                 handler.post(() -> imageView.setImageBitmap(bitmap));
+
 
             } catch (Exception e) {
                 e.printStackTrace();
